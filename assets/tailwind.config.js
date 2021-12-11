80cb246cf94f3b37a65b965ab462dc9a8fdb4e05
Arp-G <arpanghoshal3@gmail.com>
2021-12-11 19:26:57 +0530

    add custom font

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
+      // Override tailwind default "sans" font with custom font "BakBakOne"
+      // This font is defined in app.css
+      fontFamily: {
+        'sans': ['Bakbak One']
+      },
       animation: {
         'spin-slow': 'spin 3s linear infinite',
       }
